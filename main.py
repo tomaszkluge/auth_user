5094486cdaaafca8baf986950b38a16f018c6c8b
tomaszkluge <tomasz.kluge@gmail.com>
2023-09-27 14:31:16 +0200

    changed jose to PyJWT

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
@@ -7,9 +7,9 @@ from pydantic import BaseModel
 from passlib.context import CryptContext
 import jwt
 from datetime import datetime, timedelta
-from jose import JWTError, jwt
 
 app = FastAPI()
+
 
 # Root Route
 
@@ -24,6 +24,7 @@ engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+
 # User Model
 
 
@@ -35,19 +36,18 @@ class User(Base):
     password = Column(String)
 
 
-Base.metadata.ceate_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
 
 class UserCreate(BaseModel):
     username: str
     password: str
 
+
 # Register User
 
 
 @app.post("/register")
-
-
 def get_db():
     db = SessionLocal()
     try:
@@ -56,10 +56,11 @@ def get_db():
         db.close()
 
 
+@app.post("/register")
 def register_user(user: UserCreate, db: Session = Depends(get_db)):
     hashed_password = get_password_hash(user.password)
 
-    db_user = User(username=user.username, paswword=hashed_password)
+    db_user = User(username=user.username, password=hashed_password)
 
     db.add(db_user)
     db.commit()
@@ -78,6 +79,7 @@ def get_password_hash(password: str):
     pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
     return pwd_context.hash(password)
 
+
 # Generate JWT Token
 
 
@@ -86,7 +88,7 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
-def create_access_token(data: dict, expires_delta:timedelta = None):
+def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
@@ -97,7 +99,8 @@ def create_access_token(data: dict, expires_delta:timedelta = None):
     return encoded_jwt
 
 
-oauth2.scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
     try:
@@ -106,16 +109,18 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
         if username is None:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials")
         return User(username=username)
-    except JWTError:
+    except jwt.JWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials")
-
 
 
 @app.get("/protected-resource")
 def get_protected_resource(current_user: User = Depends(get_current_user)):
-    return {"message": "This is a protected resource!"}
+    # code to execute after authentication
+    return {"message": "Welcome!", "user": current_user}
+
 
 # Refresh Token
+
 
 REFRESH_TOKEN_EXPIRE_DAYS = 30
 
