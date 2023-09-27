2d4a949ba96c8cb55807c21ff339a63e1d400c64
tomaszkluge <tomasz.kluge@gmail.com>
2023-09-27 13:51:13 +0200

    Added 'refresh token'

--- main.py
+++ main.py
@@ -108,3 +108,30 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
         return User(username=username)
     except JWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials")
+
+
+
+@app.get("/protected-resource")
+def get_protected_resource(current_user: User = Depends(get_current_user)):
+    return {"message": "This is a protected resource!"}
+
+# Refresh Token
+
+REFRESH_TOKEN_EXPIRE_DAYS = 30
+
+
+def create_refresh_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+@app.post("/refresh-token")
+def refresh_token(current_user: User = Depends(get_current_user)):
+    access_token = create_access_token(data={"sub": current_user.username})
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
