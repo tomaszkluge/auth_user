a015cb307c94fb3691e72942953888b7121e674a
tomaszkluge <tomasz.kluge@gmail.com>
2023-09-27 10:40:29 +0200

    Added user Class and db

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, World!"}
+    return {"message": "Hello Space!"}
+
+
+DATABASE_URL = "sqlite:///./test/db"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    password = Column(String)
+
+
+Base.metadata.ceate_all(bind=engine)
+
