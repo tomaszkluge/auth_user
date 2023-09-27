a28cdedc05554a7698fa9dcfec6f1942502e4794
tomaszkluge <tomasz.kluge@gmail.com>
2023-09-27 11:55:27 +0200

    added registration endpoint

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
+from pydantic import BaseModel
+from passlib.context import CryptContext
+
 
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
@@ -26,3 +30,34 @@ class User(Base):
 
 Base.metadata.ceate_all(bind=engine)
 
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+
+
+@app.post("/register")
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def register_user(user: UserCreate, db: Session = Depends(get_db)):
+    hashed_password = get_password_hash(user.password)
+
+    db_user = User(username=user.username, paswword=hashed_password)
+
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+
+    return {"message": "User registered successfully!"}
+
+
+def get_password_hash(password: str):
+    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+    return pwd_context.hash(password)
